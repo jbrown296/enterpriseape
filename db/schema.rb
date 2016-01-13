8f95f77abce25e56aa2c45c4c015d958b7a1a757
JM Brown <jbrown296@yahoo.com>
2016-01-12 20:28:10 -0500

    Added static dropdown to invoices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111033946) do
+ActiveRecord::Schema.define(version: 20160113011409) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160111033946) do
     t.string   "staffperson"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status_type"
   end
 
   create_table "users", force: :cascade do |t|
