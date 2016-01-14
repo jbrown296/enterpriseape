d0973112ab9e2aa20d5a81859c239b1913d53d18
JM Brown <jbrown296@yahoo.com>
2016-01-13 20:01:16 -0500

    Added Admin dropdown to nav bar

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ActiveRecord::Base
-	belongs_to :salesperson
+	belongs_to :employee
 end
 
 # This ensures that each invoice has a salesperson attached to it. The invoices belong to a salesperson.
