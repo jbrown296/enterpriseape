13f89f318fe2235420ed6c8577f7734031099ff2
JM Brown <jbrown296@yahoo.com>
2016-01-14 13:39:48 -0500

    Built relationship btwn invoices and purchases

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ActiveRecord::Base
 	belongs_to :employee
+	has_many :purchases
 end
 
 # This ensures that each invoice has a salesperson attached to it. The invoices belong to a salesperson.
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20160114012344) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "status_type"
-    # t.integer  "salesperson_id"
     t.integer  "employee_id"
   end
 
