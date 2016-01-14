1a8569beeff9e81a068fe4f9cb4d613c2d303d7a
JM Brown <jbrown296@yahoo.com>
2016-01-14 04:15:26 -0500

    Added purchases database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113043443) do
+ActiveRecord::Schema.define(version: 20160114012344) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -37,9 +37,20 @@ ActiveRecord::Schema.define(version: 20160113043443) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "status_type"
-    # t.integer  "salesperson_id"
+    t.integer  "salesperson_id"
     t.integer  "employee_id"
   end
+
+  create_table "purchases", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.integer  "quantity"
+    t.integer  "invoice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "purchases", ["invoice_id"], name: "index_purchases_on_invoice_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
