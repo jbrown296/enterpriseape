df2152f9b7f739688a54c414ad2bf66293fd0f7f
JM Brown <jbrown296@yahoo.com>
2016-01-15 21:16:17 -0500

    Added invoice calculations

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.default_url_options = { :host => 'http://fathomless-earth-3303.herokuapp.com' }
